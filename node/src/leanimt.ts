bfc700de92eb056620217392ebd429930e8b063a
Vivian Plasencia <v.pcalana@gmail.com>
2025-09-16 23:06:57 +0200

    feat: add nodejs benchmarks for smt and leanimt

---
+++ node/src/leanimt.ts
@@ -0,0 +1,76 @@
+import { Bench } from "tinybench"
+import { LeanIMT, type LeanIMTMerkleProof } from "@zk-kit/lean-imt"
+import { poseidon2 } from "poseidon-lite"
+
+const main = async () => {
+  const bench = new Bench({
+    name: "Merkle Tree Benchmarks",
+    time: 0,
+    iterations: 100
+  })
+
+  let leanIMT: LeanIMT
+
+  let leanIMTProof: LeanIMTMerkleProof
+
+  const leanIMTHash = (a: bigint, b: bigint) => poseidon2([a, b])
+
+  bench
+    .add(
+      "LeanIMT - Insert",
+      () => {
+        leanIMT.insert(200n)
+      },
+      {
+        beforeEach: () => {
+          leanIMT = new LeanIMT(leanIMTHash)
+          leanIMT.insertMany(
+            Array.from({ length: 10 }, (_, i) => BigInt(i + 1))
+          )
+        }
+      }
+    )
+    .add(
+      "LeanIMT - Update",
+      () => {
+        leanIMT.update(0, 200n)
+      },
+      {
+        beforeEach: () => {
+          leanIMT = new LeanIMT(leanIMTHash)
+          leanIMT.insertMany(
+            Array.from({ length: 10 }, (_, i) => BigInt(i + 1))
+          )
+        }
+      }
+    )
+    .add(
+      "LeanIMT - Generate Proof",
+      () => {
+        leanIMTProof = leanIMT.generateProof(0)
+      },
+      {
+        beforeAll: () => {
+          leanIMT = new LeanIMT(leanIMTHash)
+          leanIMT.insertMany(
+            Array.from({ length: 10 }, (_, i) => BigInt(i + 1))
+          )
+        }
+      }
+    )
+    .add("LeanIMT - Verify Proof", () => {
+      leanIMT.verifyProof(leanIMTProof)
+    })
+
+  await bench.run()
+
+  console.log(bench.name)
+  console.table(bench.table())
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
---
+++ node/src/smt.ts
@@ -0,0 +1,95 @@
+import { Bench } from "tinybench"
+import {
+  Merkletree,
+  Proof,
+  verifyProof,
+  InMemoryDB,
+  str2Bytes
+} from "@iden3/js-merkletree"
+
+const main = async () => {
+  const bench = new Bench({
+    name: "Revocation Merkle Tree Benchmarks",
+    time: 0,
+    iterations: 1
+  })
+
+  let smt: Merkletree
+
+  let smtProof: Proof
+
+  bench
+    .add(
+      "SMT - Insert",
+      async () => {
+        await smt.add(200n, 200n)
+      },
+      {
+        beforeEach: async () => {
+          smt = new Merkletree(new InMemoryDB(str2Bytes("Tree")), true, 140)
+          const size = 10
+          for (let i = 0; i < size; i++) {
+            await smt.add(BigInt(i + 1), BigInt(i + 1))
+          }
+        }
+      }
+    )
+    .add(
+      "SMT - Update",
+      async () => {
+        await smt.update(2n, 2n)
+      },
+      {
+        beforeEach: async () => {
+          smt = new Merkletree(new InMemoryDB(str2Bytes("Tree")), true, 140)
+          const size = 10
+          for (let i = 0; i < size; i++) {
+            await smt.add(BigInt(i + 1), BigInt(i + 1))
+          }
+        }
+      }
+    )
+    .add(
+      "SMT - Generate Proof",
+      async () => {
+        await smt.generateProof(2n)
+      },
+      {
+        beforeEach: async () => {
+          smt = new Merkletree(new InMemoryDB(str2Bytes("Tree")), true, 140)
+          const size = 10
+          for (let i = 0; i < size; i++) {
+            await smt.add(BigInt(i + 1), BigInt(i + 1))
+          }
+        }
+      }
+    )
+    .add(
+      "SMT - Verify Proof",
+      async () => {
+        await verifyProof(await smt.root(), smtProof, 2n, 2n)
+      },
+      {
+        beforeEach: async () => {
+          smt = new Merkletree(new InMemoryDB(str2Bytes("Tree")), true, 140)
+          const size = 10
+          for (let i = 0; i < size; i++) {
+            await smt.add(BigInt(i + 1), BigInt(i + 1))
+          }
+          smtProof = (await smt.generateProof(2n)).proof
+        }
+      }
+    )
+
+  await bench.run()
+
+  console.log(bench.name)
+  console.table(bench.table())
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
