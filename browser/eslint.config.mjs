0dc27b13ab5937efd028eca2deb50992fc8db3e2
Vivian Plasencia <v.pcalana@gmail.com>
2025-09-22 22:40:23 +0200

    feat: add table component

--- browser/eslint.config.mjs
+++ browser/eslint.config.mjs
@@ -1,13 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
@@ -17,9 +17,9 @@ const eslintConfig = [
       ".next/**",
       "out/**",
       "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+      "next-env.d.ts"
+    ]
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
