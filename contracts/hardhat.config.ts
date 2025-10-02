b996e9b38bfbfe276570a1bad3f255697e32624c
Vivian Plasencia <v.pcalana@gmail.com>
2025-10-02 17:44:48 +0200

    feat: add gas reporter

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,22 @@
 import type { HardhatUserConfig } from "hardhat/config"
+import "hardhat-gas-reporter"
 import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28"
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS === "true",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
+  }
 }
 
 export default config
