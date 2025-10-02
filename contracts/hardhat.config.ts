76a230890aca9c2eda550dc3c0f32426b6333952
Vivian Plasencia <v.pcalana@gmail.com>
2025-10-02 17:39:26 +0200

    style: add prettier

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,8 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import type { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-};
+  solidity: "0.8.28"
+}
 
-export default config;
+export default config
